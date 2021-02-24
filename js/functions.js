b08dd3862a257c2411a584ac31bcd3227dd6e4e3
Amauri Edmundo Rauber Junior <amauri.rauber@gmail.com>
2021-02-24 18:47:13 +0000

    first commit

---
+++ js/functions.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('div.button').click(function(){
+		$('header').css('background','#2b2b2b')
+		$('main').css('background','black');
+		$('footer').css('background','#CCC');
+	})
+	
+})
